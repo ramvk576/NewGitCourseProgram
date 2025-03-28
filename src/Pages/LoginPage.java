f6a3c6044f7e63d74fd66dc30929ec9a2baaf06e
ramvk576 <ramvk576@gmail.com>
2025-03-29 00:08:12 +0530

    first commit

---
+++ src/Pages/LoginPage.java
@@ -0,0 +1,10 @@
+package Pages;
+
+public class LoginPage {
+	
+	public static void main(String a[])
+	{
+		System.out.println("Hello LP");
+	}
+
+}
---
+++ src/Test/LoginPageTest.java
@@ -0,0 +1,9 @@
+package Test;
+
+public class LoginPageTest {
+	
+	public void Test1() {
+		System.out.println("Test 1 - LP");
+	}
+
+}
